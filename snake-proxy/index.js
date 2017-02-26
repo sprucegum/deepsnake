a6c50ed3f4d724bd20b50e97e74a0d4d657a56e3
Jade Lacosse <jade@lacos.se>
2017-02-25 17:22:35 -0800

    Modelled the game and the snake

--- snake-proxy/index.js
+++ snake-proxy/index.js
@@ -3,47 +3,118 @@
  */
 const http = require('http');
 const _ = require('lodash');
-var Promise = require('promise');
 
 var count = 0;
 var gameState = null;
 var nextMove = "up";
-var waitForInstruction = true;
-var movePromise = null;
+var waitingForSnakeMove = false;
+var gameInstance = null;
+
+class SnakeModel {
+    constructor(snakeJSON, gameModel) {
+        this.gameModel = gameModel;
+        this.snakeJSON = snakeJSON;
+        this.name = _.get(snakeJSON, 'name');
+        this.id = _.get(snakeJSON, 'id');
+        this.coords = null;
+    }
+    updateState(snakeJSON) {
+        this.gameModel = gameModel;
+        this.snakeJSON = snakeJSON;
+        this.coords = _.get(snakeJSON, 'coords');
+        console.log("snakeCoords", this.coords);
+    }
+
+}
+
+class GameModel {
+    constructor (gameState) {
+        this.gameState = gameState;
+        this.width = _.get(gameState, 'width');
+        this.height = _.get(gameState, 'height');
+        this.you = _.get(gameState, 'you');
+        this.board = [];
+        this.snakes = {};
+        this.makeSnakes();
+        this.player = _.get(this.snakes, this.you);
+        this.populateBoard();
+    }
+    makeSnakes() {
+        var snakes = _.get(this.gameState, 'snakes');
+        snakes.map((snake) => {
+            this.snakes[snake.id] = new SnakeModel(snake, this);
+        });
+    }
+    updateSnakes() {
+        var snakes = _.get(this.gameState, 'snakes');
+        snakes.map((snake) => {
+            this.snakes[snake.id].updateState(snake);
+        });
+
+    }
+    wipeBoard() {
+        this.board = [];
+        for (var i = 0; i < this.height; i++) {
+            this.board.push(new Array(this.width).fill(0));
+        }
+    }
+    populateBoard() {
+        this.wipeBoard();
+        var player = _.get(this.gameState, 'player');
+        if (player) {
+            console.log("player", player);
+        }
+    }
+    updateState (gameState) {
+        this.gameState = gameState;
+        this.updateSnakes();
+        this.populateBoard();
+    }
+}
 
 function start(game) {
     gameState = game;
+    gameInstance = null;
+    waitingForSnakeMove = true;
     count = 0;
     console.log("start", game ,_.get(game, "test"));
     return {
-        name: 'Snek',
+        name: 'SnakeMeat',
         color: '#bb2233',
     }
 }
 
 function move(data) {
+    if (!gameInstance) {
+        gameInstance = new GameModel(data);
+    }
+    gameInstance.updateState(data);
     console.log("move", count++, data , _.get(data, "test"));
-    var move = getMove();
-    move.then(function (d) {
-        nextMove = d;
-    });
+    var move = nextMove;
+    if (waitingForSnakeMove) {
+        // if the control AI hasn't responded, then let's use a fallback AI.
+    }
+    waitingForSnakeMove = true;
     return {
         move: nextMove,
         taunt: "Boop the snoot!",
     };
 }
 
-function getMove() {
-    return new Promise(function (fulfill, reject) {
-        movePromise = fulfill;
-    });
-}
-
 function setMove(data) {
     var d = _.get(data, "d");
     console.log("set-move", data, d);
-    movePromise(d);
+    waitingForSnakeMove = false;
+    nextMove = d;
 }
+
+function getState() {
+    return {
+        gameState: gameState,
+        gameUpdated: waitingForSnakeMove
+    }
+}
+
 
 /**
  * HTTP Server
@@ -52,16 +123,16 @@ function setMove(data) {
  */
 console.log("check");
 http.createServer((req, res) => {
-    if (req.method !== 'POST') return respond(); // non-game requests
-
+    //if (req.method !== 'POST') return respond(); // non-game requests
     let body = [];
     req.on('data', chunk => body.push(chunk));
     req.on('end', () => {
         body = JSON.parse(Buffer.concat(body).toString());
-        console.log(body);
+        //console.log(body);
         if (req.url === '/start') message = start(body);
         if (req.url === '/move') message = move(body);
         if (req.url === '/set-move') message = setMove(body);
+        if (req.url === '/get-state') message = getState(body);
         return respond(message);
     });
 
