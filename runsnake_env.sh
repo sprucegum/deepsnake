44b784499820c82e2874b908ea4524af954ff58d
Jade Lacosse <jade@lacos.se>
2017-03-04 02:35:19 -0800

    added trained networks

--- runsnake_env.sh
+++ runsnake_env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t shallowsnake .
-nvidia-docker run --rm -v $(pwd)/DeepLearningVideoGames:/snake -it --entrypoint /snake/deep_q_network.py shallowsnake
+nvidia-docker run --rm -v $(pwd)/DeepLearningVideoGames:/snake --entrypoint /snake/deep_q_network.py shallowsnake
